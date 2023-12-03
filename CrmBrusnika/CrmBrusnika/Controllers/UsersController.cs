2075f50b19efdf38fcc8ec6417c5b34246528097
aterasu <obuhowdaniil902@gmail.com>
2023-12-03 16:15:10 +0500

    inital commit

---
+++ CrmBrusnika/CrmBrusnika/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrmBrusnika.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = new[]
+            {
+                new { Name = "vanya"},
+                new { Name = "ilya"}
+            };
+            return Ok(users);
+        }
+    }
+}
