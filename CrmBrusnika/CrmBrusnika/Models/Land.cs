01cd38e8735160c3e1a68a388c41a9adcc9e73f3
aterasu <obuhowdaniil902@gmail.com>
2023-12-27 18:23:10 +0500

    add model land

---
+++ CrmBrusnika/CrmBrusnika/Models/Land.cs
@@ -0,0 +1,29 @@
+﻿namespace CrmBrusnika.Models
+{
+    public class Land
+    {
+        public Guid Id { get; set; }
+        public int RegisterNumber { get; set; }
+        public string Adress { get; set; } = null!;
+        public string AreaInMeters { get; set; } = null!;
+        public string AboutHolder { get; set; } = null!;
+        public double Price { get; set; }
+        public string WhoIsFound { get; set; } = null!;
+
+        public Land(
+            int registerNumber,
+            string adress,
+            string areaInMeters,
+            string aboutHolder,
+            double price,
+            string whoIsFound)
+        {
+            RegisterNumber = registerNumber;
+            Adress = adress;
+            AreaInMeters = areaInMeters;
+            AboutHolder = aboutHolder;
+            Price = price;
+            WhoIsFound = whoIsFound;
+        }
+    }
+}
