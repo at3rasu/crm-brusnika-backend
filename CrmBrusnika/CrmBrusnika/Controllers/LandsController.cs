979ef57a631ba9b647e097ea6f0e2c0feed1131b
aterasu <obuhowdaniil902@gmail.com>
2023-12-27 19:04:38 +0500

    add post request for LandsController

--- CrmBrusnika/CrmBrusnika/Controllers/LandsController.cs
+++ CrmBrusnika/CrmBrusnika/Controllers/LandsController.cs
@@ -1,6 +1,38 @@
-﻿namespace CrmBrusnika.Controllers
+﻿using CrmBrusnika.Context;
+using CrmBrusnika.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+using System.Text.Json;
+
+namespace CrmBrusnika.Controllers
 {
+    [Route("api/lands/")]
+    [ApiController]
     public class LandsController
     {
+        private readonly LandsContext _context;
+
+        public LandsController(LandsContext context)
+        {
+            _context = context;
+        }
+
+        [Route("create")]
+        [HttpPost]
+        public async Task<Land> createLand(Land land)
+        {
+            var newLand = new Land(
+                land.RegisterNumber,
+                land.Adress,
+                land.AreaInMeters,
+                land.AboutHolder,
+                land.Price,
+                land.WhoIsFound
+            );
+            var response = await _context.AddAsync(newLand);
+            await _context.SaveChangesAsync();
+            return newLand;
+        }
     }
 }
---
+++ CrmBrusnika/CrmBrusnika/Migrations/Lands/20231227140219_AddLandContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrmBrusnika.Migrations.Lands
+{
+    /// <inheritdoc />
+    public partial class AddLandContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
