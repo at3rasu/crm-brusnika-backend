72e407e031ecbd984e9b2626eed0fe03203d8b95
aterasu <obuhowdaniil902@gmail.com>
2023-12-29 06:31:15 +0500

    edit Transaction

--- CrmBrusnika/CrmBrusnika/Models/Transaction.cs
+++ CrmBrusnika/CrmBrusnika/Models/Transaction.cs
@@ -1,10 +1,11 @@
-﻿namespace CrmBrusnika.Models
+﻿using CrmBrusnika.Models.Enums;
+
+namespace CrmBrusnika.Models
 {
     public class Transaction
     {
         public Guid Id { get; set; }
         public Land Land { get; set; } = null!;
-
-        
+        public StagesTransactions Stage { get; set; }
     }
 }
