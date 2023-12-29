50afcbe3dfb0a02f1b61396158423348cd5f757d
aterasu <obuhowdaniil902@gmail.com>
2023-12-29 06:25:51 +0500

    edit address in model Land

---
+++ CrmBrusnika/CrmBrusnika/Migrations/Lands/20231229004940_EditAddress.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrmBrusnika.Migrations.Lands
+{
+    /// <inheritdoc />
+    public partial class EditAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Adress",
+                table: "Lands",
+                newName: "Address");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "Lands",
+                newName: "Adress");
+        }
+    }
+}
