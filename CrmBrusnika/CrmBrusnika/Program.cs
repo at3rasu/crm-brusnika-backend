0d0fad8be5f7dbd492234b7e7b3114bd0a4d2e91
root <root@2355573-og08714.twc1.net>
2024-01-06 01:00:26 +0300

    edit dockerfile

--- CrmBrusnika/CrmBrusnika/Program.cs
+++ CrmBrusnika/CrmBrusnika/Program.cs
@@ -51,11 +51,11 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Example v1"));
-}
+// if (app.Environment.IsDevelopment())
+// {
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Example v1"));
+// }
 app.UseRouting();
 
 app.UseCors("CorsPolicy");
